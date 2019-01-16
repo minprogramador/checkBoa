7702531745d96cbe98599b98038d11318c958c20
minprogramador <minprogramador@gmail.com>
2019-01-16 02:36:14 -0200

    melhorias no cod

--- server.php
+++ server.php
@@ -3,49 +3,34 @@
 use React\Promise\Deferred;
 use React\ChildProcess\Process;
 use Nette\Database\Context;
+use Psr\Http\Message\ServerRequestInterface as ServReqInterf;
+use React\Socket\Server;
+use Legionth\React\Http\Rest\Server as ServerRest;
+use Nette\Database\Connection;
+use React\EventLoop\Factory;
+use React\Http\Response;
+use React\EventLoop\Timer\Timer;
 
 require_once 'vendor/autoload.php';
 
-$user = 'root';
+$user     = 'root';
 $password = '2019maconhaOk@@';
-$dsn = "mysql:host=127.0.0.1;dbname=api_boa";
-$database = new Nette\Database\Connection($dsn, $user, $password); // the same arguments as uses PDO
+$dsn      = "mysql:host=127.0.0.1;dbname=api_boa";
+$database = new Connection($dsn, $user, $password); // the same arguments as uses PDO
 
-$loop = \React\EventLoop\Factory::create();
-
-$server = new \Legionth\React\Http\Rest\Server();
-
-
-
-$loop->addPeriodicTimer(30, function(React\EventLoop\Timer\Timer $timer){
-
-	echo date("Y-m-d H:i:s")." - verifica cookie e proxys..\n";
-
-	$payload = "php main.php";
-	runPayload($payload)
-		->then(function ($value){
-			print_r($value);
-
-	    },
-	    function ($reason)  use($request, $next){
-	    	echo "\ndeu error...\n";
-			 //return new \React\Http\Response(200, array(), 'deu error');	    	
-	    }
-	);
-});
-
+$loop   = Factory::create();
+$server = new ServerRest();
 
 function runPayload($payload) {
 	global $loop;
-	$deferred = new Deferred();
-	$payload = 'php main.php';
 
-	$process = new Process($payload);
+	$deferred = new Deferred();
+	$payload  = 'php main.php';
+	$process  = new Process($payload);
 
 	$process->start($loop);
 
-	$process->stdout->on('data', function ($chunk) use (&$dados, $deferred) {
-		$resok = str_replace(['start=', '=end'], '', $chunk);
+	$process->stdout->on('data', function ($chunk) use ($deferred) {
 		$deferred->resolve($resok);
 	});
 	
@@ -54,23 +39,32 @@ function runPayload($payload) {
 	});
 
 	return $deferred->promise();
-
 }
 
-$server->get('/check', function (\Psr\Http\Message\ServerRequestInterface $request, callable $next) use($loop, $database) {
+$loop->addPeriodicTimer(30, function(Timer $timer) {
 
-	$result =  $database->fetchAll('SELECT * FROM contas');
-	$result = json_encode($result);
-	return new \React\Http\Response(200, array('Content-type' => 'application/json'), $result);
+	echo date("Y-m-d H:i:s")." - verifica cookie e proxys..\n";
+
+	$payload = "php main.php";
+	runPayload($payload)
+		->then(function ($value) {
+			print_r($value);
+	    },
+	    function ($reason) {
+	    	echo "\ndeu error...\n";
+	    }
+	);
+});
+
+$server->get('/check', function (ServReqInterf $request, callable $next) use($loop, $database) {
+
+	$result = json_encode($database->fetchAll('SELECT * FROM contas'));
+	return new Response(200, array('Content-type' => 'application/json'), $result);
+
 });
 
 
-
-// $server->get('/config', function (\Psr\Http\Message\ServerRequestInterface $request, callable $next) {
-//     return new \React\Http\Response(200, array(), 'hello');
-// });
-
-$socket = new \React\Socket\Server('0.0.0.0:5555', $loop);
+$socket = new Server('0.0.0.0:5555', $loop);
 $server->listen($socket);
 echo "run";
 $loop->run();
